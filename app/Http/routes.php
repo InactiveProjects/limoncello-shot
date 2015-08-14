b654250a379f4928cf187f2090120683fd06205f
EK <ek@neomerx.com>
2015-08-14 22:30:13 +0300

    Add limoncello

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,36 @@
 |
 */
 
+use \App\Http\Controllers\Demo\UsersController;
+
 $app->get('/', function () use ($app) {
     return $app->welcome();
 });
+
+$app->get(
+    'login/basic',
+    ['middleware' => 'jsonapi.basicAuth', 'uses' => UsersController::class.'@getSignedInUserJwt']
+);
+
+$app->get(
+    'login/refresh',
+    ['middleware' => 'jsonapi.jwtAuth', 'uses' => UsersController::class.'@getSignedInUserJwt']
+);
+
+$app->group(
+    [
+        'prefix'     => 'api/v1',
+        'middleware' => [
+            'jsonapi.jwtAuth', // comment out this line if you want to disable authentication
+        ]
+    ],
+    function () use ($app) {
+
+        include __DIR__ . '/Routes/authors.php';
+        include __DIR__ . '/Routes/comments.php';
+        include __DIR__ . '/Routes/posts.php';
+        include __DIR__ . '/Routes/sites.php';
+        include __DIR__ . '/Routes/users.php';
+
+    }
+);
