71e21f6b573e10806d9588f16b92e3b6d1f1c5f6
EK <ek@neomerx.com>
2015-08-14 22:30:13 +0300

    Add limoncello

---
+++ app/Http/Routes/posts.php
@@ -0,0 +1,24 @@
+<?php
+
+use \App\Http\Controllers\Demo\PostsController;
+
+$app->get(
+    'posts',
+    ['uses' => PostsController::class.'@index']
+);
+$app->get(
+    'posts/{id}',
+    ['uses' => PostsController::class.'@show']
+);
+$app->post(
+    '/posts',
+    ['uses' => PostsController::class.'@store']
+);
+$app->delete(
+    '/posts/{id}',
+    ['uses' => PostsController::class.'@destroy']
+);
+$app->patch(
+    '/posts/{id}',
+    ['uses' => PostsController::class.'@update']
+);
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use \App\Jwt\UserJwtCodec;
+use \App\Jwt\UserJwtCodecInterface;
+use \Illuminate\Support\ServiceProvider;
+use \App\Http\Controllers\JsonApi\LumenIntegration;
+use \Neomerx\Limoncello\Contracts\IntegrationInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,6 +17,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(IntegrationInterface::class, function () {
+            return new LumenIntegration();
+        });
+        $this->app->bind(UserJwtCodecInterface::class, function () {
+            return new UserJwtCodec();
+        });
     }
 }
